390d6f5e3d3805007d70f8fbfb8cefcec20f22ad
parsnips <michael@twisp.com>
2025-01-27 15:10:43 -0800

    blah

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/parsnips/mq
+
+go 1.23.1
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+)
+
+func respond(w http.ResponseWriter, code int, data any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	_ = json.NewEncoder(w).Encode(data)
+}
+
+func webhook(w http.ResponseWriter, r *http.Request) {
+	defer func() { _ = r.Body.Close() }()
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		respond(w, 500, map[string]any{"err": err})
+		return
+	}
+	log.Printf("request: %s\n", string(b))
+	respond(w, 200, map[string]string{})
+}
+
+func jit(w http.ResponseWriter, r *http.Request) {
+	defer func() { _ = r.Body.Close() }()
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		respond(w, 500, map[string]any{"err": err})
+		return
+	}
+	log.Printf("request: %s\n", string(b))
+	respond(w, 200, map[string]string{})
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		respond(w, 200, map[string]string{})
+	})
+	http.HandleFunc("/webhook", webhook)
+	http.HandleFunc("/jit", jit)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Printf("err: %s\n", err.Error())
+	}
+}
